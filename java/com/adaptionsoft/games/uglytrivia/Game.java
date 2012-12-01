188177db59b41476b109271f14085d95f4421992
Zsolt Takács <zsolt@takacs.cc>
2012-12-01 05:46:55 +0100

    move penalty box to players

--- java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -3,10 +3,6 @@ package com.adaptionsoft.games.uglytrivia;
 import java.util.Random;
 
 public class Game {
-    int[] purses = new int[6];
-    boolean[] inPenaltyBox = new boolean[6];
-
-    boolean isGettingOutOfPenaltyBox;
     final Questions questions = new Questions();
     final Board board = new Board();
     final Players players = new Players();
@@ -31,8 +27,8 @@ public class Game {
                 aGame.wasCorrectlyAnswered();
             }
 
-            winner = aGame.didPlayerWin();
-            aGame.nextPlayer();
+            winner = aGame.players.didPlayerWin();
+            aGame.players.nextPlayer();
 
         } while (!winner);
     }
@@ -40,8 +36,6 @@ public class Game {
     public boolean add(String playerName) {
         players.addPlayer(playerName);
         board.addPlayerToPlaces(players.howManyPlayers());
-        purses[players.howManyPlayers()] = 0;
-        inPenaltyBox[players.howManyPlayers()] = false;
 
         System.out.println(playerName + " was added");
         System.out.println("They are player number " + players.howManyPlayers());
@@ -52,9 +46,9 @@ public class Game {
         System.out.println(players.getPlayerName(players.currentPlayer()) + " is the current player");
         System.out.println("They have rolled a " + roll);
 
-        if (inPenaltyBox[players.currentPlayer()]) {
+        if (players.inPenaltyBox()) {
             if (roll % 2 != 0) {
-                isGettingOutOfPenaltyBox = true;
+                players.getOutOfPenaltyBox();
 
                 System.out.println(players.getPlayerName(players.currentPlayer()) + " is getting out of the penalty box");
                 board.movePlayer(roll, players.currentPlayer());
@@ -66,7 +60,7 @@ public class Game {
                 askQuestion();
             } else {
                 System.out.println(players.getPlayerName(players.currentPlayer()) + " is not getting out of the penalty box");
-                isGettingOutOfPenaltyBox = false;
+                players.stayInPenaltyBox();
             }
         } else {
 
@@ -81,41 +75,25 @@ public class Game {
     }
 
     public void wasCorrectlyAnswered() {
-        if (inPenaltyBox[players.currentPlayer()]) {
-            if (isGettingOutOfPenaltyBox) {
+        if (players.inPenaltyBox()) {
+            if (players.isGettingOutOfPenaltyBox()) {
                 System.out.println("Answer was correct!!!!");
-                addCoin(players.currentPlayer());
+                players.addCoin(players.currentPlayer());
             }
         } else {
             System.out.println("Answer was corrent!!!!");
-            addCoin(players.currentPlayer());
+            players.addCoin(players.currentPlayer());
         }
     }
 
     public void wrongAnswer() {
         System.out.println("Question was incorrectly answered");
         System.out.println(players.getPlayerName(players.currentPlayer()) + " was sent to the penalty box");
-        inPenaltyBox[players.currentPlayer()] = true;
-    }
-
-    private void nextPlayer() {
-        players.nextPlayer();
-    }
-
-    private boolean didPlayerWin() {
-        return (!inPenaltyBox[players.currentPlayer()] || isGettingOutOfPenaltyBox) && purses[players.currentPlayer()] == 6;
+        players.putInPenaltyBox();
     }
 
     private void askQuestion() {
         System.out.println("The category is " + board.currentCategory(players.currentPlayer()));
         System.out.println(questions.getQuestion(board.currentCategory(players.currentPlayer())));
-    }
-
-    private void addCoin(int player) {
-        purses[player]++;
-        System.out.println(players.getPlayerName(player)
-                + " now has "
-                + purses[player]
-                + " Gold Coins.");
     }
 }
--- java/com/adaptionsoft/games/uglytrivia/Players.java
+++ java/com/adaptionsoft/games/uglytrivia/Players.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class Players {
     ArrayList<String> players = new ArrayList<String>();
+    int[] purses = new int[6];
+    boolean[] inPenaltyBox = new boolean[6];
+    boolean isGettingOutOfPenaltyBox;
     int currentPlayer = 0;
 
     public Players() {
@@ -11,6 +14,8 @@ public class Players {
 
     void addPlayer(String playerName) {
         players.add(playerName);
+        purses[howManyPlayers()] = 0;
+        inPenaltyBox[howManyPlayers()] = false;
     }
 
     public int howManyPlayers() {
@@ -28,5 +33,41 @@ public class Players {
 
     String getPlayerName(int player) {
         return players.get(player);
+    }
+
+    private boolean hasEnoughCoins() {
+        return purses[currentPlayer()] == 6;
+    }
+
+    public void addCoin(int player) {
+        purses[player]++;
+        System.out.println(getPlayerName(player)
+                + " now has "
+                + purses[player]
+                + " Gold Coins.");
+    }
+
+    public boolean didPlayerWin() {
+        return (!inPenaltyBox[currentPlayer()] || isGettingOutOfPenaltyBox) && hasEnoughCoins();
+    }
+
+    public boolean inPenaltyBox() {
+        return inPenaltyBox[currentPlayer()];
+    }
+
+    public void putInPenaltyBox() {
+        inPenaltyBox[currentPlayer()] = true;
+    }
+
+    public void stayInPenaltyBox() {
+        isGettingOutOfPenaltyBox = false;
+    }
+
+    public void getOutOfPenaltyBox() {
+        isGettingOutOfPenaltyBox = true;
+    }
+
+    public boolean isGettingOutOfPenaltyBox() {
+        return isGettingOutOfPenaltyBox;
     }
 }
